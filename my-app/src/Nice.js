a3e23abf8ccac76b8e178e88216991f54955a970
qq526645879 <526645879@qq.com>
2017-05-15 14:15:42 +0800

    no message

---
+++ my-app/src/Nice.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import "./Nice.css";
+
+class Nice extends Component {
+  render() {
+    return (
+      <div className="Nice">ffff</div>
+    );
+  }
+}
+
+export default Nice;
